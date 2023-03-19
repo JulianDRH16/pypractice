8d6e74c6c7c5b02c5d0c5afb8c43ba5df9c79a26
Julian Rodriguez <julian.rh1116@gmail.com>
2023-03-19 02:14:08 -0500

    FirstPractice

---
+++ bmi.py
@@ -0,0 +1,9 @@
+mass = input("Write the mass in kilograms: ")
+mass = float(mass)
+
+height = input("Write the height in meters:")
+height = float(height)
+
+bmi= (mass/(height**2))
+
+print("The patient bim is", bmi)
