e1eacb1d19ec0d250aad973e0d6d7d8053aa1931
Julian Rodriguez <julian.rh1116@gmail.com>
2024-01-22 02:16:52 -0500

    enhacing the result with multiple scores are equal

--- dataTypes/nestedList.py
+++ dataTypes/nestedList.py
@@ -8,18 +8,19 @@
 if __name__ == '__main__':
     
     students_data = []
+    students_scores = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
         students_data.append([name, score])
+        students_scores.append(score)
 
-students_data.sort(key=lambda x : (x[1], x[0]))
-
+students_data.sort(key=lambda x : (x[1],x[0]))
+second_lowest_grade = sorted(set(students_scores))[1]
 print(students_data)
-
-second_lowest_grade = students_data[1][1]
-
 print(second_lowest_grade)
+y= type(second_lowest_grade)
+print(y)
 
 for name, score in students_data:
     if score == second_lowest_grade:
