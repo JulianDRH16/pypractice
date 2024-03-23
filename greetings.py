68cfa6dbe6fd67e02d0a6a4435953bfaaba2fd5f
Julian Rodriguez <julian.rh1116@gmail.com>
2024-03-23 00:30:23 -0500

    Greetings example challenge

---
+++ greetings.py
@@ -0,0 +1,11 @@
+
+#This is a small challenge to understand how to print a message with beautiful code in python
+
+name = input('Write your name: ')
+
+greeting = f'Hi {name}, this is not your conscience, this is something similar to an horrocrux'
+
+msg_lenght = len(greeting)
+
+print(greeting)
+print(f'The lenght of the message is {msg_lenght}')
